1e68a3c03efee82549abd5a4a0945a238bba0780
Georgee-lee <rjsahdia3@gmail.com>
2019-07-29 18:52:32 +0900

    wip in main blue background thing

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -14,8 +14,8 @@ class Main extends Component {
         <section className="tutoring_content">
           <article className="content_wrap">
             {/* 메인 image div */}
-            <div className="main_image">
-              <div className="main_text">
+            <div className="main_wrapper">
+              <div className="main_text_wrap">
                 <h3 className="main_text_title">
                   시공간 개념없는 요즘 영어
                   <em>24시간 1:1 영어회화앱</em>
@@ -25,6 +25,31 @@ class Main extends Component {
                 className="exp_btn_white"
                 onClick={this.handleExperience}
               />
+              <div className="main_underbar_wrap">
+                <ul className="main_underbar_item">
+                  <li>
+                    <span className="underbar_icon_down icon" />
+                    누적다운로드
+                    <em>200만</em>
+                  </li>
+                  <li>
+                    <span className="underbar_icon_star icon" />
+                    수강생추천율
+                    <em>96%</em>
+                  </li>
+                  <li>
+                    <span className="underbar_icon_chart icon" />
+                    연평균회원증가율
+                    <em>800%</em>
+                  </li>
+                  <li>
+                    <span className="underbar_icon_mark icon" />
+                    영어회화만족도
+                    <em>1위</em>
+                  </li>
+                </ul>
+                <p>2019년1월기준</p>
+              </div>
             </div>
 
             {/* image 밑 간략한 About div */}
