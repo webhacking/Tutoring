04a40dd5cb8e67b05dc281fd8131338c66b52644
Georgee-lee <rjsahdia3@gmail.com>
2019-08-07 16:36:25 +0900

    메인페이지 custom button component 삭제 후 a 태그로 변경

--- client/src/Pages/Main/Main.js
+++ client/src/Pages/Main/Main.js
@@ -10,14 +10,6 @@ class Main extends Component {
     fixed: false
   };
 
-  handleExperience = () => {
-    // window.open(
-    //   "https://play.google.com/store/apps/details?id=tutoring.app&referrer=utm_source%3Dgoogle%26utm_campaign%3DGPC_%EB%B8%8C%EB%9E%9C%EB%93%9C_exact%26utm_medium%3Dkeyword_pc",
-    //   "_blank"
-    // );
-    window.location.href = "/home/tutors";
-  };
-
   componentDidMount = () => {
     window.addEventListener("scroll", this.checkNavbar);
   };
@@ -46,7 +38,7 @@ class Main extends Component {
         <section className="tutoring_content">
           <article className="content_wrap">
             {/* 메인 image div */}
-            <Title onClick={this.handleExperience} />
+            <Title />
 
             {/* 한혜진 사진 밑 박스 */}
             <div className="main_about">
--- client/src/Pages/Main/MainTitle/Title.js
+++ client/src/Pages/Main/MainTitle/Title.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./title.scss";
-import ExpBtn from "Components/Button/ExperienceButton";
 import UnderbarItem from "Pages/Main/UnderbarItem";
 
-const Title = ({ onClick }) => (
+const Title = () => (
   <div className="main_image_wrapper">
     <div className="main_text_wrap">
       <h3 className="main_text_title">
@@ -11,7 +10,14 @@ const Title = ({ onClick }) => (
         <em>24시간 1:1 영어회화앱</em>
       </h3>
     </div>
-    <ExpBtn className="exp_btn_white" onClick={onClick} />
+    <a
+      href="https://play.google.com/store/apps/details?id=tutoring.app&referrer=utm_source%3Dgoogle%26utm_campaign%3DGPC_%EB%B8%8C%EB%9E%9C%EB%93%9C_exact%26utm_medium%3Dkeyword_pc"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="exp_btn_white"
+    >
+      지금 무료체험하기
+    </a>
     <div className="main_underbar_wrap">
       <UnderbarItem />
     </div>
