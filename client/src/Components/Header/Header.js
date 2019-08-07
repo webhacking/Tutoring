902fc7078cba0ee6b1cc96ba66b403191eb6b0f5
Georgee-lee <rjsahdia3@gmail.com>
2019-08-07 15:11:51 +0900

    header 완료

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -8,7 +8,8 @@ class Header extends Component {
   };
 
   state = {
-    isHover: false
+    isHover: false,
+    selectedMenu: ""
   };
 
   handleHover = () => {
@@ -23,9 +24,15 @@ class Header extends Component {
     });
   };
 
+  menuHandler = e => {
+    this.setState({
+      selectedMenu: e.target.innerHTML
+    });
+  };
+
   render() {
     const { fix } = this.props;
-    const { isHover } = this.state;
+    const { isHover, selectedMenu } = this.state;
 
     return (
       <header className={`header_wrap ${fix === true || isHover === true ? "fixed" : ""}`}>
@@ -37,11 +44,9 @@ class Header extends Component {
           </h1>
           <p className="logo_sub">24시간 1:1 영어회화앱</p>
           <nav className="gnb" onMouseOver={this.handleHover}>
-            <ul>
+            <ul onMouseOver={this.menuHandler}>
               <li>수강권보기</li>
-              <Link to="/home/tutors">
-                <li>튜터소개</li>
-              </Link>
+              <li>튜터소개</li>
               <li>토픽소개</li>
               <li>수강후기</li>
               <li>B2B프로그램</li>
@@ -49,7 +54,37 @@ class Header extends Component {
           </nav>
         </div>
         <div className={`sub_gnb ${isHover === true ? "show_sub" : ""}`} onMouseLeave={this.handleLeave}>
-          <p>여기에 서브 메뉴</p>
+          <ul className="sub_gnb_ul">
+            <li className={`sub_list sub_list_01 ${selectedMenu === "수강권보기" ? "selected" : null}`}>
+              <button type="button">기본 수강권(영어)</button>
+              <button type="button">기본 수강권(중국어)</button>
+              <img src="//cdn.tutoring.co.kr/uploads/home/img/main/version3/tudal_gnb01.png" alt="튜달이" className="sub_icon" />
+            </li>
+            <li className={`sub_list sub_list_02 ${selectedMenu === "수강권보기" && "selected"}`}>
+              <button type="button">직딩의 품격</button>
+              <button type="button">무빙마스터</button>
+              <button type="button">100일의 기적</button>
+              <button type="button">결합상품</button>
+            </li>
+            <li className={`sub_list sub_list_03 ${selectedMenu === "튜터소개" && "selected"}`}>
+              <Link to="/home/tutors">
+                <button type="button">영어 튜터</button>
+              </Link>
+              <button type="button">중국어 튜터</button>
+              <img src="//cdn.tutoring.co.kr/uploads/home/img/main/version3/tudal_gnb02.png" alt="튜달이" className="sub_icon" />
+            </li>
+            <li className={`sub_list sub_list_04 ${selectedMenu === "토픽소개" && "selected"}`}>
+              <button type="button">영어 토픽</button>
+              <button type="button">중국어 토픽</button>
+              <button type="button">카드 뉴스</button>
+              <button type="button">Editor's Pick</button>
+              <img src="//cdn.tutoring.co.kr/uploads/home/img/main/version3/tudal_gnb03.png" alt="튜달이" className="sub_icon" />
+            </li>
+            <li className={`sub_list sub_list_05 ${selectedMenu === "B2B프로그램" && "selected"}`}>
+              <button type="button">기업 수강/제휴 할인</button>
+              <img src="//cdn.tutoring.co.kr/uploads/home/img/main/version3/tudal_gnb05.png" alt="튜달이" className="sub_icon" />
+            </li>
+          </ul>
         </div>
       </header>
     );
